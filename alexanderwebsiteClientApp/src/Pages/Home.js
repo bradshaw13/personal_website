cab232e4fbb6a42907c6252ff225d1f32f5c398b
Alexander Bradshaw <alexanderbradshaw@Alexanders-MacBook-Pro.local>
2021-09-01 23:15:08 -0400

    added a button

--- alexanderwebsiteClientApp/src/Pages/Home.js
+++ alexanderwebsiteClientApp/src/Pages/Home.js
@@ -1,10 +1,42 @@
 import React from 'react'
-
+import background from "../images/brycenational.jpeg";
+import './Home.css';
 function Home() {
     return (
-        <div className='home'>
-            <h1>Home</h1>
+        <div>
+            <div className="background">
+                <div className='home'>
+                    <h1>Hello, my name is Alexander Bradshaw </h1>
+                </div>
+                <div className='home'>
+                    <h1>Explore my interests! </h1>
+                </div>
+                <div className='home'>
+                    <button className='testButton'>
+                        About Me
+                    </button>
+                </div>
+
+            </div>
+            <div className="background">
+                <div className='home'>
+                    <h1>Hello, my name is Alexander Bradshaw </h1>
+                </div>
+                <div className='home'>
+                    <h1>Explore my interests! </h1>
+                </div>
+                <div className='home'>
+                    <button className='testButton'>
+                        About Me
+                    </button>
+                </div>
+
+            </div>
         </div>
+       
+        
+
+     
     )
 }
 
