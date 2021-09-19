68d68161f669b0f0d299367710050ed4c0a3c60d
Alexander Bradshaw <alexanderbradshaw@Alexanders-MBP.lan>
2021-09-19 11:38:08 -0500

    made a server for api

--- alexanderwebsiteClientApp/src/Pages/AboutMe.js
+++ alexanderwebsiteClientApp/src/Pages/AboutMe.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './AboutMe.css';
-
+import axios from 'axios';
 function AboutMe() {
-    
+    const [test, setTest] = useState('');
+    useEffect(() => {
+        axios.get('/api/v1/say-something').then((res) => {
+            const response = res.data.body;
+            setTest(response);
+        });
+        console.log("this do be the request")
+    }, []);
+
     return (
         <div className='aboutMe'>
             <h1 className="title">AboutMe</h1>
             <div className="aboutMeParagraph">My name is Alexander Cuellar Bradshaw </div>
+            <h1>{test}</h1>
         </div>
     )
 }
